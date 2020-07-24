0a48db25519cbb630a87b3e95811de7f2bcbcba3
Steve Twist <stevetwist@gmail.com>
2020-07-24 14:57:01 -0700

    Modify launch script

--- Run_Caster_DNS.bat
+++ Run_Caster_DNS.bat
@@ -1,9 +1,12 @@
 @echo off
 echo Runnig DNS/DPI from Dragonfly CLI with Natlink.
 
+C:
+cd C:\Caster
+
 set currentpath=%~dp0
 
 TITLE Caster: Status Window
-python -m dragonfly load --engine natlink _*.py --no-recobs-messages
+"C:\Program Files\PythonEnvironments\caster\python.exe" -m dragonfly load --engine natlink _*.py --no-recobs-messages
 
 pause 1
