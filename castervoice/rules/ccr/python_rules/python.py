1bdbc90f531455ae1361d8617948460e00a2a08f
Steve Twist <stevetwist@gmail.com>
2020-07-28 15:41:49 -0700

    Finish refining Python commands

--- castervoice/rules/ccr/python_rules/python.py
+++ castervoice/rules/ccr/python_rules/python.py
@@ -4,9 +4,11 @@
 Note: commands that utilize code snippets are designed for use with VisualStudio Code
 and the Python extension for VS Code.
 '''
+import time
+
 from dragonfly import Choice, Function
 
-from castervoice.lib.actions import Text
+from castervoice.lib.actions import Key, Text
 from castervoice.lib.const import CCRType
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
 from castervoice.lib.merge.mergerule import MergeRule
@@ -39,23 +41,23 @@ basicKeywordMap = {
 }
 
 codeSnippetMap = {
-    "from" : "from",
-    "class" : "class", # TODO: Code snippet: Also include def __init__(self):\n\tpass
-    "derived class" : "derived class", # TODO: Code snippet: Also include def __init__(self):\n\t<BASE CLASS>.__init__(self)
-    "function" : "function",
-    "method" : "method",
-    "static method" : "static method",
-    "see method" : "class method",
-    "if" : "if",      
-    "L if" : "elif",
-    "else" : "else",        
-    "for" : "for",        
-    "while" : "while",      
-    "try" : "try",
-    "except" : "except",
-    "finally" : "finally",
-    "with" : "with",# TODO: Code snippet: with <TOKEN> as <TOKEN>:
-    "short with" : "short with", # TODO: Code snippet: with <TOKEN>:
+    "from" : "st_cs_from",
+    "class" : "st_cs_class",
+    "derived class" : "st_cs_derived_class",
+    "function" : "st_cs_function",
+    "method" : "st_cs_method",
+    "static method" : "st_cs_static_method",
+    "see method" : "st_cs_class_method",
+    "if" : "st_cs_if",
+    "L if" : "st_cs_elif",
+    "else" : "st_cs_else",
+    "for" : "st_cs_for",
+    "while" : "st_cs_while",
+    "try" : "st_cs_try",
+    "except" : "st_cs_except",    
+    "finally" : "st_cs_finally",
+    "with" : "st_cs_with",
+    "short with" : "st_cs_short_with",
 }
 
 
@@ -73,8 +75,21 @@ def _basicKeyword(basicKeyword):
     
     
 def _codeSnippet(codeSnippet):
-    print ("TODO: Execute code snippet: %s" % codeSnippet)
-
+    snippetId = codeSnippetMap[codeSnippet]
+    commandChain = []
+    for character in snippetId:
+        commandChain.append(Key(character))        
+    
+    command = commandChain[0]
+    for c in commandChain[1:]:
+        command += c
+        
+    R(command).execute()
+    
+    time.sleep(0.5) # Wait for intellisense
+    
+    R(Key('tab')).execute()
+    
 
 class Python(MergeRule):
 
---
+++ steve/INSTALL_README.txt
@@ -0,0 +1,4 @@
+Copy ./vs_code/snippets/* to C:\Users\<username>\AppData\Roaming\Code\User\snippets\
+    e.g. ./vs_code/snippets/python.json -> C:\Users\<username>\AppData\Roaming\Code\User\snippets\python.json
+
+After first run of Caster, copy rules.toml to C:\Users\<username>\AppData\Local\caster\settings\rules.toml
