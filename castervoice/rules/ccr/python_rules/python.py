a3a63f07bb24bc7fe1b22b0bbe80f9e25d50a055
Steve Twist <stevetwist@gmail.com>
2020-07-28 11:30:56 -0700

    Continue reworking Python commands

--- castervoice/rules/ccr/python_rules/python.py
+++ castervoice/rules/ccr/python_rules/python.py
@@ -4,83 +4,97 @@
 Note: commands that utilize code snippets are designed for use with VisualStudio Code
 and the Python extension for VS Code.
 '''
-from dragonfly import Pause, Dictation, Choice
+from dragonfly import Choice, Function
 
-from castervoice.lib.actions import Text, Key
-from castervoice.rules.ccr.standard import SymbolSpecs
+from castervoice.lib.actions import Text
 from castervoice.lib.const import CCRType
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
 from castervoice.lib.merge.mergerule import MergeRule
 from castervoice.lib.merge.state.short import R
-from castervoice.lib.temporary import Store, Retrieve
+
+basicKeywordMap = {
+    "bit shift left" : " << ",
+    "bit shift right" : " >> ",
+    "bitwise and" : " & ",
+    "bitwise or" : " | ",
+    "bitwise complement" : "~",
+    "bitwise (exclusive|ex) or" : " ^ ",
+    "and" : " and ",
+    "as" : " as ",
+    "break" : "break ",
+    "continue" : "continue ",
+    "pie deli" : "del ",
+    "false" : "False",
+    "global" : "global ",
+    "in" : " in ",
+    "import" : "import ",
+    "is" : " is ",
+    "none" : "None",
+    "not" : "not ",
+    "or" : " or ",
+    "pass" : "pass ",
+    "raise" : "raise ",
+    "return" : "return ",
+    "true" : "True",    
+}
+
+codeSnippetMap = {
+    "from" : "from",
+    "class" : "class", # TODO: Code snippet: Also include def __init__(self):\n\tpass
+    "derived class" : "derived class", # TODO: Code snippet: Also include def __init__(self):\n\t<BASE CLASS>.__init__(self)
+    "function" : "function",
+    "method" : "method",
+    "static method" : "static method",
+    "see method" : "class method",
+    "if" : "if",      
+    "L if" : "elif",
+    "else" : "else",        
+    "for" : "for",        
+    "while" : "while",      
+    "try" : "try",
+    "except" : "except",
+    "finally" : "finally",
+    "with" : "with",# TODO: Code snippet: with <TOKEN> as <TOKEN>:
+    "short with" : "short with", # TODO: Code snippet: with <TOKEN>:
+}
+
+
+def _getChoiceMap(mapVariable):
+    choiceMap = {}
+    for key in mapVariable:
+        choiceMap[key] = key
+        
+    return choiceMap
+
+
+def _basicKeyword(basicKeyword):
+    text = basicKeywordMap[basicKeyword]
+    R(Text(text)).execute()
+    
+    
+def _codeSnippet(codeSnippet):
+    print ("TODO: Execute code snippet: %s" % codeSnippet)
 
 
 class Python(MergeRule):
 
     mapping = { 
-        "bit shift left":
-            R(Text(" << ")),
-        "bit shift right":
-            R(Text(" >> ")),
-        "bitwise and":
-            R(Text(" & ")),
-        "bitwise or":
-            R(Text(" | ")),
-        "bitwise complement":
-            R(Text(" ~")),
-        "bitwise (exclusive|ex) or":
-            R(Text(" ^ ")),
-        "and":
-            R(Text(" and ")),    
-        "break":
-            R(Text("break ")),
-        "continue":
-            R(Text("continue ")),
-        "pie deli": # py deli
-            R(Text("del ")),
-        "false":
-            R(Text("False")),        
-        "in":
-            R(Text(" in ")),
-        "is":
-            R(Text(" is ")),
-        "none":
-            R(Text("None")),
-        "not":
-            R(Text("not ")),        
-        "or":
-            R(Text(" or ")),    
-        "pass":
-            R(Text("pass ")),        
-        "true":
-            R(Text("True")),
-        
-        # import (simple word "import ")
-        # as -> simple text (" as ")
-        # global -> simple text "global "
-        # raise -> simple text ("raise ")
-        # return -> (simple text ("return ")
-        
-        # from -> should be a code snippet: from <token> import <token>
-        # class -> code snippet
-        # derived class -> code snippet
-        # method, static method, class method, function -> code snippets
-        # if, elif, else -> code snippets
-        # try-except, try-finally, try-except-finally -> code snippets
-        # for -> code snippet: for <TOKEN> in <TOKEN>:\n<TOKEN=pass>        
-        # while -> code snippet
-        # with -> code snippet (with <TOKEN> as <TOKEN>:)
-        # short with -> code snippet (with <TOKEN>:)
-        # helpers?
-        #   init -> "__init__"
-        #   "dunder" -> "__" (possibly in punctuation)
+        "<basicKeyword>" :
+            R(Function(_basicKeyword)),
+        "<codeSnippet>" :
+            R(Function(_codeSnippet)),
+     
+        # VisualStudio Code Helpers
         #
         # VS Code Helpers
         #   line <number>
         #   eventually: debugging helpers (step in, step over, step out, debug continue, etc.)
+        #   eventually: tab navigation
+        #   ideally: "go to definition" for text under cursor (or, failing that, for hovered text)
     }
-
-    extras = [        
+    extras = [  
+        Choice("basicKeyword", _getChoiceMap(basicKeywordMap)),
+        Choice("codeSnippet", _getChoiceMap(codeSnippetMap))
     ]
     defaults = {}
 
