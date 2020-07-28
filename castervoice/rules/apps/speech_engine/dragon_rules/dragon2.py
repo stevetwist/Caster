42a6a910c92c2b3b972e3295bf206b1eedb5074a
Steve Twist <stevetwist@gmail.com>
2020-07-28 16:42:42 -0700

    Remove broken dragon commands

--- castervoice/rules/apps/speech_engine/dragon_rules/dragon2.py
+++ castervoice/rules/apps/speech_engine/dragon_rules/dragon2.py
@@ -11,13 +11,13 @@ class SpellingWindowRule(MappingRule):
     mapping = {
         # TODO: make these CCR
 
-        "<first_second_third> word":
-            R(Key("home, c-right:%(first_second_third)d, cs-right"),
-              rdescript="Dragon: select the first second or third etc. word"),
-        "last [word]": R(Key("right, cs-left"), rdescript="Dragon: select the last word"),
-        "second [to] last word": R(Key("right, c-left:1, cs-left"), rdescript="Dragon: select the second to last word"),
-        "<n10>": R(Mimic("choose", extra="n10"),
-                   rdescript="Dragon: e.g. instead of having to say 'choose two' you can just say 'two'"),
+        #"<first_second_third> word":
+        #    R(Key("home, c-right:%(first_second_third)d, cs-right"),
+        #      rdescript="Dragon: select the first second or third etc. word"),
+        #"last [word]": R(Key("right, cs-left"), rdescript="Dragon: select the last word"),
+        #"second [to] last word": R(Key("right, c-left:1, cs-left"), rdescript="Dragon: select the second to last word"),
+        #"<n10>": R(Mimic("choose", extra="n10"),
+        #           rdescript="Dragon: e.g. instead of having to say 'choose two' you can just say 'two'"),
         # consider making the above command global so that it works when you say something like
         # "insert before 'hello'" where there are multiple instances of 'hello'
         # personally I think it's better just to have the setting where Dragon choose is the closest instance
